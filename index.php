0eeea20cd8fda2460fb9ebf20aac913585ce2eaf
Willian <wln16@bol.com.br>
2013-11-08 22:57:47 -0200

    nd

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <html>
 	<head>
-
-	</head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        
+        <link href="plugins/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+        <script src="http://code.jquery.com/jquery-2.0.2.min.js"></script>
+        <script src="plugins/bootstrap/js/bootstrap.min.js" > </script>
+        </head>
     <body>
-    	will
-    </body>
+    	<div class="dropdown">
+  		<a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+  <!-- Link or button to toggle dropdown -->
+  <ul class="dropdown-menu">
+    <li><a tabindex="-1" href="#">Ação</a></li>
+    <li><a tabindex="-1" href="#">Outra ação</a></li>
+    <li><a tabindex="-1" href="#">Algo a mais aqui</a></li>
+    <li class="divider"></li>
+    <li><a tabindex="-1" href="#">Link separador</a></li>
+  </ul>
+</div>
+</body>
 </html> 
+
+
+	
