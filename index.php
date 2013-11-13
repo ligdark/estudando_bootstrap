c995ddfdf777a5cf0ab124dc9b2bfcb5e7f6cd43
Willian <wln16@bol.com.br>
2013-11-13 00:23:42 -0200

    teste

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
         <link href="plugins/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
         <script src="http://code.jquery.com/jquery-2.0.2.min.js"></script>
         <script src="plugins/bootstrap/js/bootstrap.min.js" > </script>
-        </head>
+    </head>
     <body>
     	<div class="dropdown">
   		<a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
+  	wwwww
   <!-- Link or button to toggle dropdown -->
   <ul class="dropdown-menu">
     <li><a tabindex="-1" href="#">Ação</a></li>
