2d78ae98e04953e83d2ffedc56f3e34d9ab8844a
Willian <wln16@bol.com.br>
2013-11-13 21:39:28 -0200

    d

---
+++ Carousel/index.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        
+        <link href="../plugins/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+        <script src="http://code.jquery.com/jquery-2.0.2.min.js"></script>
+        <script src="../plugins/bootstrap/js/bootstrap.min.js" > </script>
+    	
+    </head>
+    <body>
+        <div class="container">
+        	<div class="row">
+            	<div class="span6 well">
+                	<div id="myCarousel" class="carousel">
+                    	<div class="carousel-inner">
+                        	<div class="item active"><img src="imgs/1 (1).jpg"></div>
+                            <div class="item"><img src="imgs/1 (2).jpg"></div>
+                            <div class="item"><img src="imgs/1 (3).jpg"></div>
+                            <div class="item"><img src="imgs/1 (4).jpg"></div>
+                            <div class="item"><img src="imgs/1 (5).jpg"></div>
+                            <div class="item"><img src="imgs/1 (6).jpg"></div>
+                        </div>
+                        <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                        <a class="carousel-control right" href="#myCarousel" data-slide="prev">&rsaquo;</a> 
+                    </div>
+                </div> 
+            </div>
+        </div>
+        <script>
+			$('.carousel').carousel();
+		</script>
+	</body>
+</html> 
+
+
+	
