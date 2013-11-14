3b6578d9c9bb15b764212c73e98a91ef5317d559
Willian <wln16@bol.com.br>
2013-11-14 02:17:42 -0200

    Criando menu Navbar dropdown

--- DropDownMenu/index.php
+++ DropDownMenu/index.php
@@ -1,46 +1,46 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Documento sem título</title>
-	<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
-    <link href="../plugins/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
-    
-    <!--<link href="../plugins/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">-->
-    <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
-    <script src="../plugins/bootstrap/js/bootstrap.js" > </script>
-    
-</head>
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Documento sem título</title>
+  	<!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+      <link href="../plugins/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+      
+      <!--<link href="../plugins/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">-->
+      <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+      <script src="../plugins/bootstrap/js/bootstrap.js" > </script>
+      
+  </head>
 
-<body>
-	<div class="container">
-        <div class="row">
-            <div class="span12">
-                <h3> Bootstrap DropDown Menu </h3>
-                
-                <nav class="navbar navbar-default" role="navigation">
-                    <div class="navbar-nav">
-                        <ul class="nav">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Home</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            
-  <!-- Brand and toggle get grouped for better mobile display -->
-  <!--<div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="#">Brand</a>
-  </div>-->
-  
-        </div>
-    </div>
-</body>
-</html>
+  <body>
+  	<div class="container">
+          <div class="row">
+              <div class="span12">
+                  <h3> Bootstrap DropDown Menu </h3>
+                  
+                  <nav class="navbar navbar-default" role="navigation">
+                        <div class="navbar">
+                          <ul class="nav">
+                              <li><a href="#">Home</a></li>
+                              <li><a href="#">Home</a></li>
+                              <li><a href="#">Home</a></li>
+                          </ul>
+                      </div>
+                  </div>
+              </nav>
+              
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <!--<div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>-->
+    
+          </div>
+      </div>
+  </body>
+  </html>
---
+++ Website/index.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../plugins/bootstrap/css/bootstrap.css">
+	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+    <script src="../plugins/bootstrap/js/bootstrap.js" > </script>
+</head>
+<body>
+
+	<div class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
+				<a href="#" class="navbar-brand">Willian</a>
+			</div>
+
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Tutorials <b class="caret"></b></a>
+					
+						<ul class="dropdown-menu">
+							<li><a href="#">Bootstrap Tutorials</a></li>
+							<li><a href="#">PHP Tutorials</a></li>
+							<li><a href="#">Java Tutorials</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	
+</body>
+</html>
