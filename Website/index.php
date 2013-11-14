1bd10475a29d4879de58e2100e5de108d0348f8b
Willian <wln16@bol.com.br>
2013-11-14 19:48:23 -0200

    Carousel 14-11-2013 - 19:48

--- Website/index.php
+++ Website/index.php
@@ -38,5 +38,72 @@
 		</div>
 	</div>
 	
+	<!-- <div class="container span2"> -->
+		<div id="myCarousel" class="carousel slide">
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>
+
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="../imgs/2.png">
+
+					<div class="container">
+						<div class="carousel-caption">
+							<h1>My Heading Text</h1>
+							<p>This tag will contain the text whitch we want to appear on our slide</p>
+							<p><a href="#" class="btn btn-large btn-primary">Sign Up</a></p>
+						</div>
+					</div>
+				</div>
+
+				<div class="item">
+					<img src="../imgs/3.png">
+
+					<div class="container">
+						<div class="carousel-caption">
+							<h1>My Heading Text</h1>
+							<p>This tag will contain the text whitch we want to appear on our slide</p>
+							<p><a href="#" class="btn btn-large btn-primary">Sign Up</a></p>
+						</div>
+					</div>
+				</div>
+
+				<div class="item">
+					<img src="../imgs/4.png">
+
+					<div class="container">
+						<div class="carousel-caption">
+							<h1>My Heading Text</h1>
+							<p>This tag will contain the text whitch we want to appear on our slide</p>
+							<p><a href="#" class="btn btn-large btn-primary">Sign Up</a></p>
+						</div>
+					</div>
+				</div>
+
+				<div class="item">
+					<img src="../imgs/5.png">
+
+					<div class="container">
+						<div class="carousel-caption">
+							<h1>My Heading Text</h1>
+							<p>This tag will contain the text whitch we want to appear on our slide</p>
+							<p><a href="#" class="btn btn-large btn-primary">Sign Up</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<a href="#myCarousel" class="left carousel-control" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+			</a>
+
+			<a href="#myCarousel" class="right carousel-control" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+			</a>
+		</div>
+	<!-- </div> -->
 </body>
 </html>
