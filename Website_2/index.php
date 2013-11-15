6704889399dfb71818ea1e96c2dd1048530a5f99
Willian <wln16@bol.com.br>
2013-11-15 01:47:17 -0200

    Website 2

---
+++ Website_2/index.php
@@ -0,0 +1,119 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../plugins/bootstrap/css/bootstrap.css">
+	<!-- <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet"> -->
+	<link rel="stylesheet" type="text/css" href="../plugins/font-awesome/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+    <script src="../plugins/bootstrap/js/bootstrap.js" > </script>
+</head>
+<body>
+
+	<!-- Menu -->
+	<!-- ============ -->
+
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
+				<a href="#" class="navbar-brand">Willian</a>
+			</div>
+
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="home.php">Home</a></li>
+					<li><a href="about.php">Blog</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Social Media <b class="caret"></b></a>
+					
+						<ul class="dropdown-menu">
+							<li><a href="#">Bootstrap Tutorials</a></li>
+							<li><a href="#">PHP Tutorials</a></li>
+							<li><a href="#">Java Tutorials</a></li>
+						</ul>
+					</li>
+					<li><a href="../paginas/contact.php">About</a></li>
+					<li><a href="../paginas/contact.php">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</div><br /><br /><br />
+
+	<div class="container">
+		<div class="jumbotron">
+			<h1>Hello World</h1>
+			<p>Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here</p>
+		</div>
+	</div>
+
+	<!-- Grid -->
+	<!-- ============ -->
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<h2>First Heading</h2>
+				<p>Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here </p>
+			</div>
+
+			<div class="col-md-3">
+				<h2>First Heading</h2>
+				<p>Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here </p>
+			</div>
+
+			<div class="col-md-3">
+				<h2>First Heading</h2>
+				<p>Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here </p>
+			</div>
+
+			<div class="col-md-3">
+				<h2>First Heading</h2>
+				<p>Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here Some text will go here </p>
+			</div>
+
+		</div>
+
+	</div>
+
+	<!-- Modal -->
+	<!-- ============ -->
+
+	<div class="container">
+		<div class="row">
+			<div class="col=md-12">
+
+				<hr>
+				<p>
+					Copyright &copy; LigDark.
+					<a data-toggle="modal" href="#myModal">Termos e Condições</a>
+				</p>
+
+				<!-- Modal -->
+				<div class="modal fade" id="myModal" tobinex="-1" role="dialog" aria-hidden="true">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h1>Termos e Condições</h1>
+							</div>
+							
+							<div class="modal-body">
+								<p>The text will go here...</p>
+							</div>
+							
+							<div class="modal-footer">
+								<button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
